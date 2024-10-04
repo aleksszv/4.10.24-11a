84a502c332569b466e6f2af943a1ea81df6e9812
aleksszv <alekss.zvinklis00@gmail.com>
2024-10-04 11:39:00 +0300

    4.10.24

---
+++ untitled0.py
@@ -0,0 +1,33 @@
+a = 5
+
+b = 10
+
+print(a + b)
+
+print(a - b)
+
+print(a * b)
+
+print(a / b)
+
+print(a ** b)
+
+print(a // b)
+
+print(a % b)
+
+a = int(input('Enter number a: '))
+
+print(a + b)
+
+print(a - b)
+
+print(a * b)
+
+print(a / b)
+
+print(a ** b)
+
+print(a // b)
+
+print(a % b)
